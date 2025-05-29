e6d3017adf2571f090d4162ffe61d13f27281af5
Jacob Gross <sig@howlpoint.com>
2025-05-28 20:09:27 -0600

    Data Types

---
+++ JavaScript/Data Types/Data Types.js
@@ -0,0 +1,35 @@
+// string: anything inside " " or ' '
+"76 helloshsh"
+'65'
+
+//Number: Literally just numbers 
+
+1234
+    -9
+0
+    -9.8
+
+//boolean: Either true or false (only in lowercase)
+
+true
+false 
+
+//true = 1
+//false = 0
+
+//undefiined: we don't have anything 
+
+//null: When you wanna state when something doesn't exist or empty 
+
+//Object: Anthing inside { }
+
+{
+    x: 1
+}
+
+//NaN: Not a number 
+
+
+//most important atm: string, number, boolean, undefined, null, object, NaN
+
+console.log(typeof 1); //Will print out what type of Data Type 
---
+++ JavaScript/Your First JS Program/Your First JS Program.js
@@ -0,0 +1,12 @@
+<script>
+    <script src="Intro to JS"></script> //connecting this file to the Intro to JS file 
+</script>
+
+//A ; means you've ended the operation 
+
+console.log("hello world"); //creates text
+
+console.warn("this is a waring"); //creates waring 
+
+console.error("this is an error"); //creates an error 
+

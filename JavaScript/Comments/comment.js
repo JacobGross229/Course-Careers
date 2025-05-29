d04c507508b3d78efd61693ba6b61efab3578025
Jacob Gross <sig@howlpoint.com>
2025-05-28 20:32:39 -0600

    Variables

--- JavaScript/Comments/comment.js
+++ JavaScript/Comments/comment.js
@@ -1,3 +1,22 @@
 // This is a comment!
 //Comments are ignored 
 console.log("below the comment"); // this prints some stuff to the console 
+// good practice is adding a space between the // and the first word
+
+/*
+This is a super long comment
+words
+are
+hard
+to 
+come
+up
+with
+*/
+
+// cmd forward-slash
+
+// console.log("Words");
+// console.log("Words");
+// console.log("Words");
+// console.log("Words");
---
+++ JavaScript/Variables/Variables.js
@@ -0,0 +1,52 @@
+// Variable: container that is storing some kind of value 
+
+var x = 1; // variable x is equal to 1 
+console.log(x);
+
+// Use a descriptive name for variables 
+// Spaces not allowed 
+// Use Camel Case ex: myNumber theNumber
+// Can use underscores 
+// Can't start with a number 
+// Var is case sensitive
+
+var myNumber = 1;
+
+console.log(myNumber);
+
+myNumber = 2;
+
+console.log(myNumber);
+
+// Var can change as the program goes on 
+
+myNumber = myNumber + 2;
+
+console.log(myNumber);
+
+// many Var
+
+var name1 = "Jake";
+var name2 = "Cody";
+
+console.log(name, name2, 1);
+
+name2 = name1;
+console.log(name1, name2);
+
+name2 = "jill"
+console.log(name1, name2);
+
+
+// Constant variable: not gonna change  
+
+const myNumber = 22;
+
+// Let: allows numbers to change 
+
+let myNumber1 = 3;
+
+let myNumber2 = null;
+
+// Avoid using "var"
+

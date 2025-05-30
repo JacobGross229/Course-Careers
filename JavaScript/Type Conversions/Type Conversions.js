4f6489620b9dc44f3cb8f5f3fb2af5ac9790d0e0
Jacob Gross <sig@howlpoint.com>
2025-05-29 18:54:19 -0600

    Type Conversions

---
+++ JavaScript/Type Conversions/Type Conversions.js
@@ -0,0 +1,35 @@
+const x = "5";
+const y = 10;
+const restult = x + y;
+
+//console.log(result);
+
+// concadinations is when you add 2 strings together 
+// "hello" + "helllo"
+
+// const result = y + Number(x); // Number converts string to a number 
+
+// Can only convert numbers not letters 
+
+// const result = y + parseInt(x); // parseInt converts whole numbers 
+
+// const result = y + parseFloat // converts decimal numbers 
+
+// False converts to 0
+// True converts to 1
+
+// converting things to strings 
+
+const name = "jake";
+const age = 24;
+const year = "2000";
+
+const sentence = "Hi my name is " + name + " and I am" + age.toString + " years old";
+
+console.log(typeof age);
+
+console.log(string(age), age);
+
+constole.log((2 + 3).toString()); // Numbers must be in ()
+
+console.log(+year + 23); // automatically convert string to a number 

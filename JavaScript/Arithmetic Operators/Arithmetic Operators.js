0b7c1ee28a65045884c375f3978e89eb410f0a71
Jacob Gross <sig@howlpoint.com>
2025-05-29 18:32:48 -0600

    Arithmetic Operators

---
+++ JavaScript/Arithmetic Operators/Arithmetic Operators.js
@@ -0,0 +1,32 @@
+/*
++ Add
+- Subtract 
+* Multiply
+/ Divide
+** Raise one number to the power of another 
+% Remainder 
+++ Add 1
+-- Subtract 1
+*/
+
+const x = 10;
+const y = 20;
+
+const z = (x + y) + 10 / 2;
+console.log(z);
+
+// Follows PEMDAS
+
+// const z = x ** y;
+
+// const z = x % y;
+
+// cannot divide by 0 
+
+// let x = 10;
+// let y = 4;
+
+x++; // adds 1 to x
+x--; // subtracts 1 to x 
+
+// const z = 10 + (x ** 2) - (4 * 7);

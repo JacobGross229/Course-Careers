7115eba8d074cdcbddf1e87d59edcfe556608c28
Jacob Gross <sig@howlpoint.com>
2025-05-29 19:29:14 -0600

    Logical Operators

---
+++ JavaScript/Logical operators/Logical Operators.js
@@ -0,0 +1,20 @@
+/*
+&& and: Tell us if both the left and the right are the same 
+|| or: Tell us if both the left or the right is the same 
+! Not: Reverse what the boolean is 
+*/
+
+// const and = x && y && z; 
+// console.log(and);
+
+const or = true || true;
+
+console.log(or);
+
+// const not = !true;
+const not = !(5 < 4);
+
+const and = (6 + 2 < 9) && (-9 + 27 % 3 == 2);
+
+console.log(and); 
+

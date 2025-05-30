d3d0a72b182a203ad31e4c9c8eb224ed5b4dd5f1
Jacob Gross <sig@howlpoint.com>
2025-05-29 18:39:08 -0600

    Assignment Operators

---
+++ JavaScript/Assignment Operators/Assignment Operators.js
@@ -0,0 +1,12 @@
+let num = 10;
+num += 6; // num = num + 6
+
+console.log(num);
+
+num -= 6; // num = num - 6
+
+num *= 6; // num = num * 6
+
+num %= 6; // num = num % 6 
+
+num **= 6; // num = num ** 6

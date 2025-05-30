eca923335b19c95cc88ac3a4a7f8f0ce9f038f55
Jacob Gross <sig@howlpoint.com>
2025-05-29 19:12:35 -0600

    Comparison Operators

---
+++ JavaScript/Comparison Operators/Comparison Operators.js
@@ -0,0 +1,35 @@
+/*
+== seeing if something is equal to something. This is a loose equivilance.
+=== strict equal to. Checks if the value and type is the same.
+!= Not equal to 
+!=== Strict not equal to 
+>  greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+*/
+
+// const value = 5 == 5;
+
+console.log(value);
+
+// const value = 5 === 5;
+// === is case sensitive 
+// === is the recommended
+
+// const value = 5 < 5; // (true 
+// const value = 5 < 6; // false 
+
+const value = "a" > "b"; 
+
+console.log(value);
+
+// "A" = 65
+// "a" = 97
+
+const str = "hello"
+console.log(str.charCodeAt(0)) // find the character code 
+
+// Don't compare 2 numaric strings. It will look at the first number.
+
+// ascii codes

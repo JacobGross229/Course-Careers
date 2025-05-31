d7cba936028ae4c8c5ab3a9dc3879e6112dc2556
Jacob Gross <sig@howlpoint.com>
2025-05-31 17:29:04 -0600

    Strings

---
+++ JavaScript/Strings/Strings.js
@@ -0,0 +1,11 @@
+console.log("\" ' ");
+console.log('\\');
+console.log('Hello\nWorld'); // creates Hello (next line) World 
+console.log("Hello\n\tWorld") // \t means tab
+
+const str = `hello ${6 + 7} world`; // ${} prints out a normal JS program
+
+const name1 = "Jake";
+const str1 = `hello ${name1} world`;
+
+console.log(str1.length);

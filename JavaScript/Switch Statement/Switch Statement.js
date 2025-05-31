2bacbe517554faf4cce83d96b222681e0357d47d
Jacob Gross <sig@howlpoint.com>
2025-05-31 16:32:01 -0600

    Switch Statements

---
+++ JavaScript/Switch Statement/Switch Statement.js
@@ -0,0 +1,35 @@
+const color = prompt("Enter your fav color: ");
+
+switch (color) {
+    case "red":
+        alert("red ");
+        break;
+
+    case "blue":
+        alert("blue is my fav colois just ok");
+        break;
+
+    case "orange":
+        alert("orange is my least fav color");
+        break;
+
+    default:
+        alert("I don't know that color");
+        break;    
+}
+
+const age = Number(prompt("Enter your age"));
+
+switch(age) {
+    case 10:
+        alert("You are 10");
+        break;
+
+    case 20:
+        alert("You are 20");
+        break;
+
+    case 30:
+        alert("You are 30");
+        break;
+}

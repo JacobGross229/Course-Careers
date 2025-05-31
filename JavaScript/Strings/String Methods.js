e9208cc169fd08ee61c83e473ef867ae0c73e9f2
Jacob Gross <sig@howlpoint.com>
2025-05-31 17:48:18 -0600

    String Methods

---
+++ JavaScript/Strings/String Methods.js
@@ -0,0 +1,34 @@
+const str = "hello world";
+const str2 = str.slice(6);
+console.log(str2); // Prints world 
+
+const str3 = "hello world";
+const str4 = str.replace("hello", "!");
+console.log(str4); // Prints ! world
+
+const str5 = "hello world";
+const str6 = str.replaceAll("l", "!");
+console.log(str6); // Replaces all l's with !
+
+const str7 = "hello world";
+const str8 = str.toUpperCase();
+console.log(str8); // Makes everything uppercase
+
+const str9 = "hello world";
+const str0 = str.toLowerCase();
+console.log(str0); // Makes everything lowercase
+
+if (str.toLowerCase == "yes");
+
+const str11 = "hello world";
+const str12 = str.trim();
+console.log(str12, str11); // Removes any empty space from the beginning or end 
+
+str.trimStart(); // Removes space at the beginning 
+
+str.trimEnd(); // Removes space at the end 
+
+const str13 = "hello world";
+const arr14 = str.split(" "); // seperates string into array 
+console.log(str13);
+console.log(arr14); 

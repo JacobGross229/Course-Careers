5f3430dd76255c8de3c4bde64f81fc5c5cc8baba
Jacob Gross <sig@howlpoint.com>
2025-06-01 19:26:25 -0600

    Maps

---
+++ JavaScript/Maps/Maps.js
@@ -0,0 +1,51 @@
+const m = new Map();
+m.set("a", 3);
+m.set("b", 2);
+m.set("a", 4);
+
+console.log(m.get("a"))
+
+console.log(m.has(4));
+
+console.log(m.delete(4));
+
+console.log(m.size);
+
+console.log(m);
+
+// ------------------------------------------------------------------------------
+
+// inefficiant way 
+
+const str = "Hello world my name is Jake"
+const freq = [];
+
+for (const char of str) {
+    let found = false;
+    for (const [idx, pair] of freq.enteries) {
+        const [key, value] = pair;
+        if (key === char) {
+            freq[idx] = [char, value + 1];
+            found = true;
+            break;
+        }
+    }
+    if (!found) freq.push([char, 1])
+}
+
+console.log(freq);
+
+// ------------------------------------------------------------------------------
+
+const str1 = "Hello world my name is Jake";
+const freq1 = new Map();
+
+for (const char of str) {
+    if (freq1.has(char)) {
+        freq1.set(char, freq1.get(char) + 1)
+    } else {
+        freq1.set(char, 1);
+    }
+}
+
+console.log(freq1.get(" "));

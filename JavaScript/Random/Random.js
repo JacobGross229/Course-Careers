b2290d7b1df0414444959fb2db20eb7843d68852
Jacob Gross <sig@howlpoint.com>
2025-06-05 19:47:26 -0600

    Random

---
+++ JavaScript/Random/Random.js
@@ -0,0 +1,50 @@
+// const num = Math.round(Math.random() * 10) // Random number between 1 and 10
+// console.log(num);
+
+// ------------------------------------------------------------------------------
+
+// const num1 = 10 + Math.round(Math.random() * 10) // Random number between 10 and 20
+// console.log(num);
+
+// ------------------------------------------------------------------------------
+
+// function randomNum(min, max) {
+//     return Math.floor(Math.randiom() * (max - min)) + min;
+// }
+// 
+// const num2 = randomNum(20, 80);
+// console.log(num2);
+
+// ------------------------------------------------------------------------------
+
+function randomNum(min, max) {
+    return Math.floor(Math.randiom() * (max - min)) + min;
+}
+
+let num = randomNum(1, 100);
+let numGueses = 0;
+
+function reset () {
+    num = randomNum(1, 100);
+    numGueses = 0;
+    document.getElementById("num-guesses").innerHTML = 0;
+}
+
+function makeguess () {
+    const guessInput = document.getElementById("guess");
+    const guess = Number(guessInput);
+    guessInput.value = "";
+    const feedback = document.getElementById("feedback");
+
+numGueses++;
+document.getElementById("num-guesses").innerHTML = numGueses;
+
+    if (guess === num) {
+        feedback.innerHTML = "You guessed it" 
+        reset();        
+    } else if (guess > num) {
+        feedback.innerHTML = "It is lower";
+    } else {
+        feedback.innerHTML = "It is higher"
+    }
+}
